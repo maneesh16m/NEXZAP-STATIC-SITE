5721a6801f45c59aea9281f6822aabdb50feb85f
Maneesh16n <maneesh16n@gmail.com>
2024-11-22 16:24:54 +1100

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,35 @@
+export const ProductsData  = [
+  {
+    title: "WALLET",
+    description: "A custom-built gift card management system with dedicated admin & customer portal. Easily create, distribute, and track both physical and digital gift cards to engage customers and drive sales",
+    href: "/wallet"
+  },
+  {
+    title: "PRIVATEBOT",
+    description:"A cutting-edge, LLM-based model designed to help companies organize and retrieve information efficiently. With PRIVATEBOT, businesses can leverage AI for intelligent document search and processing.",
+    href: "/privatebot"
+  },
+  {
+    title: "TAGFLO",
+    description:"TAGFLO, is a comprehensive tracking tool for businesses that need to manage and monitor their valuable assets. TAGFLO, ensures accountability through item verification, PIN-based access, and photo capture.",
+    href: "/tagflo"
+  },
+];
+  
+  
+  export const Services= [
+
+    {
+      id:1,
+      title: "Support Service",
+      description: "Our dedicated support team provides round-the-clock assistance for our products, ensuring you get the help you need 24/7..",
+    },
+    {
+      id:2,
+      title: "Application Development",
+      description: "We specialize in creating tailored software applications that meet your business's unique needs. From automation to full-scale platforms, we deliver innovative solutions that scale with your growth.",
+    },
+    
+  ];
+  
+  
